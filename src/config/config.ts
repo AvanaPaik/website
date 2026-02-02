b8b8bb8ef2841543e79a9054cf1b181176a39cfc
AvanaPaik <avanapaik@gmail.com>
2026-02-02 14:58:53 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -148,7 +148,7 @@ const siteConfig = {
   ],
 
   contact: {
-    email: 'your.email@example.com',
+    email: 'avanapaik@gmail.com',
     linkedin: 'https://www.linkedin.com/in/your-linkedin/',
     github: 'https://github.com/your-username',
     googleScholar: 'https://scholar.google.com/citations?user=YOURID',
