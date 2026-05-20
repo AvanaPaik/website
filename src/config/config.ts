5804ea0d72426a3a2f64905126710fd6a12b94ca
AvanaPaik <avanapaik@gmail.com>
2026-05-20 21:49:40 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -108,8 +108,8 @@ const siteConfig = {
   projects: [
     {
       title: 'Project Title 1',
-      cardImage: getAsset('images/project/placeholder.png'),
-      description: 'Short description of your project and what it does...',
+      cardImage: getAsset('images/project/macro_cia3.pdf'),
+      description: 'Conducted a comparative analysis of India and Japan using key macroeconomic indicators including inflation, government expenditure, lending interest rates, GDP growth, and GDP per capita to examine differences in fiscal policy, monetary conditions, and economic development trajectories.',
       Githublink: 'https://github.com/your-username/your-project',
     },
     {
